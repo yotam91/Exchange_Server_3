40cf22e49b04671a3f20085fe0d35280b75ced9f
yotam91 <52106121+yotam91@users.noreply.github.com>
2021-07-27 23:14:24 +0300

    Update models.py

--- models.py
+++ models.py
@@ -1,191 +1,45 @@
-from flask import Flask, request, g
-from flask_restful import Resource, Api
-from sqlalchemy import create_engine, select, MetaData, Table
-from flask import jsonify
-import json
-import eth_account
-import algosdk
-from sqlalchemy.orm import sessionmaker
-from sqlalchemy.orm import scoped_session
-from sqlalchemy.orm import load_only
-from pprint import pprint
-import verification_endpoint
-from models import Base, Order, Log
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy import Column, ForeignKey, Integer, String, Table, Float, DateTime, Enum
+from sqlalchemy.orm import relationship
+from sqlalchemy import create_engine
+from datetime import datetime
+from sqlalchemy.orm import backref
+Base = declarative_base()
+
+PLATFORMS = ['Algorand','Ethereum']
+
+class Order(Base):
+    __tablename__ = 'orders'
+    id = Column(Integer,primary_key=True)
+    receiver_pk = Column( String(256) )
+    sender_pk = Column( String(256) )
+    tx_id = Column(String(256))
+    buy_currency = Column(Enum(*PLATFORMS))
+    sell_currency = Column(Enum(*PLATFORMS))
+    buy_amount = Column(Integer,default=0)
+    sell_amount = Column(Integer,default=0)
+    signature = Column(String(256))
+    timestamp = Column(DateTime,default=datetime.now()) #When was the order inserted into the database
+    counterparty_id = Column(Integer,ForeignKey('orders.id')) #If the order was filled (or partially filled) the order_id of the order that acted as the counterparty
+    counterparty = relationship("Order",foreign_keys='Order.counterparty_id') #The Order object referenced by counterparty_id
+    filled = Column(DateTime) #When was the order filled
+    creator_id = Column(Integer,ForeignKey('orders.id')) #For derived orders: the order ID of the order that this order was derived from
+    child = relationship("Order", foreign_keys='Order.creator_id', backref=backref('creator', remote_side=[id])) #set "child" and "creator" to be the derived Order and creating Order objects associated. 
+
+class TX(Base):
+    __tablename__ = 'txes'
+    id = Column(Integer,primary_key=True)
+    platform = Column(Enum(*PLATFORMS))
+    receiver_pk = Column(String(256))
+    order_id = Column(Integer,ForeignKey('orders.id')) #The id of the order that 
+    order = relationship("Order", foreign_keys='TX.order_id' )
+    tx_id = Column(String(256))
+
+class Log(Base):
+    __tablename__ = 'log'
+    id = Column(Integer,primary_key=True)
+    logtime = Column(DateTime,default=datetime.now())
+    message = Column(String(1000))
 
 engine = create_engine('sqlite:///orders.db')
-Base.metadata.bind = engine
-DBSession = sessionmaker(bind=engine)
-session = DBSession()
-
-app = Flask(__name__)
-
-
-# These decorators allow you to use g.session to access the database inside the request code
-@app.before_request
-def create_session():
-    g.session = scoped_session(
-        DBSession)  # g is an "application global" https://flask.palletsprojects.com/en/1.1.x/api/#application-globals
-
-
-@app.teardown_appcontext
-def shutdown_session(response_or_exc):
-    g.session.commit()
-    g.session.remove()
-
-
-"""
--------- Helper methods (feel free to add your own!) -------
-"""
-
-
-def log_message(d):
-    # Takes input dictionary d and writes it to the Log table
-    payload = d['payload']
-    messagePayload = json.dumps(payload)
-    log_obj = Log(message=messagePayload)
-    session.add(log_obj)
-    session.commit()
-    pass
-
-
-"""
----------------- Endpoints ----------------
-"""
-
-
-@app.route('/trade', methods=['POST'])
-# delete content
-def trade():
-    if request.method == "POST":
-        content = request.get_json(silent=True)
-        print(f"content = {json.dumps(content)}")
-        columns = ["sender_pk", "receiver_pk", "buy_currency", "sell_currency", "buy_amount", "sell_amount", "platform"]
-        fields = ["sig", "payload"]
-        error = False
-        for field in fields:
-            if not field in content.keys():
-                print(f"{field} not received by Trade")
-                print(json.dumps(content))
-                log_message(content)
-                return jsonify(False)
-
-        error = False
-        for column in columns:
-            if not column in content['payload'].keys():
-                print(f"{column} not received by Trade")
-                error = True
-        if error:
-            print(json.dumps(content))
-            log_message(content)
-            return jsonify(False)
-
-        # Your code here
-        platform = content['payload']['sell_currency']
-        if platform == 'Ethereum':
-            if verifyEth(content):
-                insert_order_to_db(content)
-                return jsonify(True)
-        elif platform == 'Algorand':
-            if verifyAlg(content):
-                insert_order_to_db(content)
-                return jsonify(True)
-        else:
-            log_message(content)
-        return jsonify(False)
-
-
-
-@app.route('/order_book')
-def order_book():
-    # Your code here
-    # Note that you can access the database session using g.session
-    #
-    orders = session.query(Order).filter(Order.filled == None).all()
-    data = []
-    for existing_order in orders:
-        if is_valid_order(existing_order):
-            #print("valid")
-            sender_pk = existing_order.sender_pk
-            receiver_pk = existing_order.receiver_pk
-            buy_currency = existing_order.buy_currency
-            sell_currency = existing_order.sell_currency
-            buy_amount = existing_order.buy_amount
-            sell_amount = existing_order.sell_amount
-            signature = existing_order.signature
-
-            orderJson = {
-                "sender_pk": sender_pk,
-                "receiver_pk": receiver_pk,
-                "buy_currency": buy_currency,
-                "sell_currency": sell_currency,
-                "buy_amount": buy_amount,
-                "sell_amount": sell_amount,
-                "signature": signature,
-            }
-            data.append(orderJson)
-    result = {"data": data}
-    # return json.dumps(result)
-    # return app.response_class(json.dumps(result), content_type='application/json')
-    return jsonify(result)
-
-
-def insert_order_to_db(order):
-    order_obj = Order(sender_pk=order['payload']['sender_pk'], receiver_pk=order['payload']['receiver_pk'],
-                      buy_currency=order['payload']['buy_currency'], sell_currency=order['payload']['sell_currency'],
-                      buy_amount=order['payload']['buy_amount'], sell_amount=order['payload']['sell_amount'])
-    session.add(order_obj)
-    session.commit()
-    # print("order inserted ", order)
-
-
-def is_valid_order(existing_order):
-    sig = existing_order.signature
-    sender_pk = existing_order.sender_pk
-    receiver_pk = existing_order.receiver_pk
-    buy_currency = existing_order.buy_currency
-    sell_currency = existing_order.sell_currency
-    buy_amount = existing_order.buy_amount
-    sell_amount = existing_order.sell_amount
-    platform = sell_currency
-
-    if sell_currency != "Ethereum" and sell_currency != "Algorand":
-        return False
-
-    return True
-
-
-def verifyEth(content):
-    valid_eth = False
-    eth_pk = content['payload']['sender_pk']
-    payload = content['payload']
-    payload = json.dumps(payload)
-    eth_encoded_msg = eth_account.messages.encode_defunct(text=payload)
-    if eth_account.Account.recover_message(eth_encoded_msg, signature=content['sig']) == eth_pk:
-        print("Eth sig verifies!")
-        valid_eth = True
-    return valid_eth
-
-
-def verifyAlg(content):
-    payload = content['payload']
-    algo_sig_str = content['sig']
-    algo_pk = payload['sender_pk']
-    payload = json.dumps(payload)
-    if algosdk.util.verify_bytes(payload.encode('utf-8'), algo_sig_str, algo_pk):
-        print("Algo sig verifies!")
-        return True
-    return False
-
-
-if __name__ == '__main__':
-    #content = {'sig':'0xd51babaaefe9cacbfae8bed46434399ad05700403ee7aa51c83ddb5d04ab7d4d4cb5307263812ea0c002cf6aebed2c0677adf0d74529d68ca59bbdb81ccdd4da1c', 'payload': {'platform': 'Ethereum', 'sender_pk': '0x906DB3244Eea11815EABEf36f8bcbbF47C92765C', 'receiver_pk': 'a651c757d3488953430969a31d480d4bf8cb4e16952ed41fee1feef764ee3df6', 'buy_currency': 'Algorand', 'sell_currency': 'Ethereum', 'sell_amount': 5172, 'buy_amount': 15516}}
-    #verifyAlg(content)
-    app.run(port='5002')
-    #order_book()
-#        'sig': '0xd51babaaefe9cacbfae8bed46434399ad05700403ee7aa51c83ddb5d04ab7d4d4cb5307263812ea0c002cf6aebed2c0677adf0d74529d68ca59bbdb81ccdd4da1c',
-#        'payload': {'platform': 'Ethereum', 'sender_pk': '0x906DB3244Eea11815EABEf36f8bcbbF47C92765C',
-#                    'receiver_pk': 'a651c757d3488953430969a31d480d4bf8cb4e16952ed41fee1feef764ee3df6',
-#                    'buy_currency': 'Algorand',
-#                    'sell_currency': 'Ethereum',
-#                    'sell_amount': 5172,
-#                    'buy_amount': 15516}}
+Base.metadata.create_all(engine)
